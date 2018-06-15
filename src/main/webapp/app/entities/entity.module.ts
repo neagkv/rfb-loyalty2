c5d198b785261dc543ad7b65b2d08a53a9040821
neagkv <neagkv@gmail.com>
2018-06-15 00:03:05 -0400

    jdl import

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { Rfbloyalty2RfbLocationModule } from './rfb-location/rfb-location.module';
+import { Rfbloyalty2RfbEventModule } from './rfb-event/rfb-event.module';
+import { Rfbloyalty2RfbEventAttendanceModule } from './rfb-event-attendance/rfb-event-attendance.module';
+import { Rfbloyalty2RfbUserModule } from './rfb-user/rfb-user.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        Rfbloyalty2RfbLocationModule,
+        Rfbloyalty2RfbEventModule,
+        Rfbloyalty2RfbEventAttendanceModule,
+        Rfbloyalty2RfbUserModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
